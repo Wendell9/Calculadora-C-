b3e6cd6544a9acf93dd1c3a7ae8b059862da6648
Wendell Moreira Guindani <113864252+Wendell9@users.noreply.github.com>
2023-05-10 15:05:54 -0300

    Primeira commit

---
+++ Calculadora simples/Calculadora simples/Operacoes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_simples
+{
+    internal static class Operacoes
+    {
+        public static double soma(double a, double b) 
+        { 
+            return a+b; 
+        }
+
+        public static double multiplicacao(double a, double b)
+        {
+            return a*b;
+        }
+
+        public static double divisao(double a,double b)
+        {
+            double resultado2;
+            resultado2 = a / b;
+            return resultado2;
+        }
+    }
+}
---
+++ Calculadora simples/Calculadora simples/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int posicao;
+            double a, b;
+            double resultado = 0;
+            string operacao = Console.ReadLine();
+            Console.WriteLine("Digite a operação desejada:");
+        Inicio:
+            if (operacao.IndexOf('*') > operacao.IndexOf('/'))
+            {
+                if (operacao.Contains('*'))
+                {
+                    posicao = operacao.IndexOf("*");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.multiplicacao(a, b);
+                    string c = operacao.Substring(posicao - 1, 3);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                else if (operacao.Contains('/'))
+                {
+                    posicao = operacao.IndexOf("/");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.divisao(a, b);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                else if (operacao.Contains('+'))
+                {
+                    posicao = operacao.IndexOf("+");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.soma(a, b);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                Console.WriteLine(resultado);
+            }
+            else
+            {
+                if (operacao.Contains('/'))
+                {
+                    posicao = operacao.IndexOf("/");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.divisao(a, b);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                else if (operacao.Contains('*'))
+                {
+                    posicao = operacao.IndexOf("*");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.multiplicacao(a, b);
+                    string c = operacao.Substring(posicao - 1, 3);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                else if (operacao.Contains('+'))
+                {
+                    posicao = operacao.IndexOf("+");
+                    a = double.Parse(operacao.Substring(posicao - 1, 1));
+                    b = double.Parse(operacao.Substring(posicao + 1, 1));
+                    resultado = Operacoes.soma(a, b);
+                    operacao = operacao.Replace(operacao.Substring(posicao - 1, 3), resultado.ToString());
+                    goto Inicio;
+                }
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+}
